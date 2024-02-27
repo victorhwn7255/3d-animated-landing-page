f272075461002bfd8de5ce50ec2186c79b4f8d6d
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2024-02-27 10:19:11 +0800

    -add character

--- src/App/world/Character.js
+++ src/App/world/Character.js
@@ -1,10 +1,13 @@
 import * as THREE from "three";
+import assetStore from "../Utils/AssetStore.js";
 
 import App from "../App.js";
 export default class Character {
   constructor() {
     this.app = new App();
     this.scene = this.app.scene;
+    this.assetStore = assetStore.getState()
+    this.avatar = this.assetStore.loadedAssets.avatar
 
     // create character and add to scene
     const geometry = new THREE.BoxGeometry(2, 2, 2);
@@ -15,5 +18,9 @@ export default class Character {
     this.instance = new THREE.Mesh(geometry, material);
     this.instance.position.set(0, 2.5, 0);
     this.scene.add(this.instance);
+
+    const avatar = this.avatar.scene
+    avatar.scale.setScalar(3)
+    this.instance.add(avatar)
   }
 }
